ba378b032a7c011a2b0c53b9904a5e94ad646bbc
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-07-09 16:08:41 +0530

    Scoobi doobi doo

--- nvim/init.lua
+++ nvim/init.lua
@@ -102,8 +102,8 @@ cmp.setup({
 require('lualine').setup {
   options = {
     theme = 'tokyodark',
-     section_separators = { left = '', right = '' },
-    component_separators = { left = '', right = '' },
+     section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
   },
   sections = {
     lualine_a = {'mode'},   
