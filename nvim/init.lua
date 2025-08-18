01732fe576162ea2fd19300e534304110784d255
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-08-18 22:38:20 +0530

    Set colorscheme priority to 1000

--- nvim/init.lua
+++ nvim/init.lua
@@ -30,8 +30,8 @@ require("lazy").setup({
   { 'folke/noice.nvim' },
   { 'akinsho/bufferline.nvim', version = "*", dependencies = { 'nvim-tree/nvim-web-devicons' }},
   { 'folke/flash.nvim' },
-  { 'folke/tokyonight.nvim' },
-  { 'catppuccin/nvim', name="catppuccin" }
+  { 'folke/tokyonight.nvim', priority=1000 },
+  { 'catppuccin/nvim', name="catppuccin", priority=1000}
 })
 
 -- Configuring plugins, tweak these as you want
